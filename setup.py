66f954d72b7849fcd208a66d87d181cd1e2b08bc
Shelby Shum <sshum@shelbys-mbp.lan>
2021-12-13 18:06:14 -0800

    added scaffolding

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gopro_merger',
+      version='0.1',
+      description='Helps merge gopro files',
+      url='https://github.com/sillyfatcat/gopro-merger',
+      author='Shelby Shum',
+      author_email='flyingcircus@example.com',
+      license='Apache License 2.0',
+      packages=['gopro_merger'],
+      zip_safe=False)
